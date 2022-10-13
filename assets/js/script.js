17bdc063a3b3b33246ab293a34422a1dffb8b184
IKEOFFIAH PIUS <piuspio12@gmail.com>
2022-10-13 23:06:38 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+'use strict';
+
+/**
+ * navbar variables
+ */
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+
+  });
+
+}
+
+
+
+
+
+
+/**
+ * header sticky functionality
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 20 ? header.classList.add("active") : header.classList.remove("active");
+  
+
+});
+
+
+
+
+
+
+/**
+ * Auto typing functionality
+ */
+
+var typed = new Typed(".typing", {
+  strings: ["Mobile Apps", "Websites", "UI/UX"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+  showCursor:false
+});
+
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 800 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
